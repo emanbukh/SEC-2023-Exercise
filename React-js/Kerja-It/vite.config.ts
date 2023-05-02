5d8851f8fc8d6541ea2447ca68a14e46439c51a2
emanbukh <emanbukhori@gmail.com>
2023-05-02 17:14:12 +0800

    add

--- React-js/Kerja-It/vite.config.ts
+++ React-js/Kerja-It/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SEC-2023-Exercise/React-js/Kerja-It"
+  base: "/SEC-2023-Exercise/React-js/Kerja-It/"
 })
